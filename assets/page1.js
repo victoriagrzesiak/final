41747e1e60bdc286b4c5acd54c5cfd8492c55466
Victoria Grzesiak <72040821+victoriagrzesiak@users.noreply.github.com>
2020-12-17 00:55:55 -0600

    Add files via upload

---
+++ assets/page1.js
@@ -0,0 +1,185 @@
+$(function() {
+  $(".title_button").hover(function() {
+    $(".display_projected").toggleClass("class");
+  });
+});
+
+
+$(function() {
+  $(".hello_world").hover(function() {
+    $(".display_projected").toggleClass("world");
+  });
+});
+
+$(function() {
+  $(".century_project").hover(function() {
+    $(".display_projected").toggleClass("century");
+  });
+});
+
+$(function() {
+  $(".fan_project").hover(function() {
+    $(".display_projected").toggleClass("fan");
+  });
+});
+
+$(function() {
+  $(".app_project").hover(function() {
+    $(".display_projected").toggleClass("muse");
+  });
+});
+
+$(function() {
+  $(".coding").hover(function() {
+    $(".display_projected").toggleClass("life");
+  });
+});
+
+$(function() {
+  $(".hovering").hover(function() {
+    $(".display_projected").toggleClass("hover");
+  });
+});
+
+$(function() {
+  $(".css_project").hover(function() {
+    $(".display_projected").toggleClass("animation");
+  });
+});
+
+$(function() {
+  $(".nav_project").hover(function() {
+    $(".display_projected").toggleClass("nav");
+  });
+});
+
+$(function() {
+  $(".type1_project").hover(function() {
+    $(".display_projected").toggleClass("type1");
+  });
+});
+
+$(function() {
+  $(".type2_project").hover(function() {
+    $(".display_projected").toggleClass("type2");
+  });
+});
+
+$(function() {
+  $(".media_project").hover(function() {
+    $(".display_projected").toggleClass("media");
+  });
+});
+
+$(function() {
+  $(".jquery1_project").hover(function() {
+    $(".display_projected").toggleClass("jquery1");
+  });
+});
+
+$(function() {
+  $(".jquery2_project").hover(function() {
+    $(".display_projected").toggleClass("jquery2");
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(function() {
+  $(".title_button").hover(function() {
+    $(".display_info").toggleClass("class");
+  });
+});
+
+
+$(function() {
+  $(".hello_world").hover(function() {
+    $(".display_info").toggleClass("world");
+  });
+});
+
+$(function() {
+  $(".century_project").hover(function() {
+    $(".display_info").toggleClass("century");
+  });
+});
+
+$(function() {
+  $(".fan_project").hover(function() {
+    $(".display_info").toggleClass("fan");
+  });
+});
+
+$(function() {
+  $(".app_project").hover(function() {
+    $(".display_info").toggleClass("muse");
+  });
+});
+
+$(function() {
+  $(".coding").hover(function() {
+    $(".display_info").toggleClass("life");
+  });
+});
+
+$(function() {
+  $(".hovering").hover(function() {
+    $(".display_info").toggleClass("hover");
+  });
+});
+
+$(function() {
+  $(".css_project").hover(function() {
+    $(".display_info").toggleClass("animation");
+  });
+});
+
+$(function() {
+  $(".nav_project").hover(function() {
+    $(".display_info").toggleClass("nav");
+  });
+});
+
+$(function() {
+  $(".type1_project").hover(function() {
+    $(".display_info").toggleClass("type1");
+  });
+});
+
+$(function() {
+  $(".type2_project").hover(function() {
+    $(".display_info").toggleClass("type2");
+  });
+});
+
+$(function() {
+  $(".media_project").hover(function() {
+    $(".display_info").toggleClass("media");
+  });
+});
+
+$(function() {
+  $(".jquery1_project").hover(function() {
+    $(".display_info").toggleClass("jquery1");
+  });
+});
+
+$(function() {
+  $(".jquery2_project").hover(function() {
+    $(".display_info").toggleClass("jquery2");
+  });
+});
+
+
